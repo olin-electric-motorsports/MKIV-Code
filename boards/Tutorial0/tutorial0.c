b12acbf1209b83ed1abfe8a98a54cc510500540b
Alexander Hoppe <alexanderehoppe@gmail.com>
2019-02-28 17:16:56 -0500

    Remove macros from tutorial0

--- boards/Tutorial0/tutorial0.c
+++ boards/Tutorial0/tutorial0.c
@@ -9,37 +9,30 @@
 
 #include <avr/io.h>
 
-/* Definitions based on board pins */
-#define LED_PIN     PB0     
-#define PINOUT_1    PB6
 
-
-int main (void) {                       /* This part of the code runs once */
+int main (void) { /* This part of the code runs once */
 
     /* Set up output pins (various ways) */
 
-    // DDRB = DDRB | 0b00000001 // Using | and binary port access
-    // DDRB = DDRB | _BV(PB0);  // Using | and pre-defined name         
-    DDRB |= _BV(LED_PIN);       // Using |= and pin name macro (most legible)
+    // DDRB = DDRB | 0b00000001     // Using | and binary port access
+    DDRB = DDRB | _BV(PB0);         // Using | and pre-defined name         
     
 
     /* Set up input pin with pull-up resistor */
-    DDRB &= ~_BV(PINOUT_1);     /* Set pin to input (unnecessary, pins
+    DDRB &= ~_BV(PB6);          /* Set pin to input (unnecessary, pins
                                  * default to input */
 
-    PORTB |= _BV(PINOUT_1);     /* Set output high (if DDR is set to input, 
+    PORTB |= _BV(PB6);          /* Set output high (if DDR is set to input, 
                                  * this sets a pull-up resistor */
 
 
-
-
-    while (1) {                     /* This part of the code runs forever */
+    while (1) { /* This part of the code runs forever */
         
-        // if (PINB & _BV(PINOUT_1)) {             /* Alternate way of writing */
-        if (bit_is_set(PINB, PINOUT_1)) {       // If the Pinout1 pin is high
-            PORTB &= ~_BV(LED_PIN);             // Turn LED off
-        } else {                                // Otherwise
-            PORTB |= _BV(LED_PIN);              // Turn it on
+        // if (PINB & _BV(PB6)) {           /* Alternate way of writing */
+        if (bit_is_set(PINB, PB6)) {        // If the Pinout1 pin is high
+            PORTB &= ~_BV(PB0);             // Turn LED off
+        } else {                            // Otherwise
+            PORTB |= _BV(PB0);              // Turn it on
         }
     }                                   
 }
