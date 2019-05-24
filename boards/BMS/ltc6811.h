743a3c69bb32c67c96726edc8bcbd37274bd80bc
Alexander Hoppe <alexanderehoppe@gmail.com>
2019-05-24 19:10:20 -0400

    BMS Voltage sense works, temperature sensing does not.

--- boards/BMS/ltc6811.h
+++ boards/BMS/ltc6811.h
@@ -19,14 +19,28 @@
 #include "ltc6811_defs.h"
 
 #define TOTAL_IC    1
-#define NUM_CELLS   12 //TODO shouldn't this be 10?
+#define NUM_CELLS   12 //There are 10, but we need to read all 12 back.
+#define NUM_TEMPS   10 //There are 10 of these.
+#define NUM_AUX_CH  6   // There are 10 AUX voltages
 
 // Cell voltages read back from the peripheral board
 // LSbit = 0.0001V, i.e. 65535 -> 6.5535V
 // |  cell_voltages[0][0]| cell_voltages[0][1] |  cell_voltages[0][2]|    .....     |  cell_voltages[0][11]|  cell_voltages[1][0] | cell_voltages[1][1]|  .....   |
 // |---------------------|---------------------|---------------------|--------------|----------------------|----------------------|--------------------|----------|
 // |IC1 Cell 1           |IC1 Cell 2           |IC1 Cell 3           |    .....     |  IC1 Cell 12         |IC2 Cell 1            |IC2 Cell 2          | .....    |
-uint16_t raw_cell_voltages[TOTAL_IC][NUM_CELLS];
+uint16_t cell_voltages[TOTAL_IC][NUM_CELLS];
+
+// AUX voltages read back from the peripheral board
+// LSbit = 0.0001V
+// Order is     |IC1 GPIO1 |IC1 GPIO2 |IC1 GPIO3 |IC1 GPIO4 |IC1 GPIO5 |IC1 Vref2 |
+uint16_t _aux_voltages[TOTAL_IC][NUM_AUX_CH];
+
+//Thermistor divider voltages read back from the peripheral board
+// LSbit = 0.0001V, i.e. 65535 -> 6.5535V
+// |  ts_voltages[0][0]  |  ts_voltages[0][1]  |  ts_voltages[0][2]  |    .....     |
+// |---------------------|---------------------|---------------------|--------------|
+// |IC1 module1 t1       |IC1 module1 t2       |IC1 module2 t1       |    .....     |
+uint16_t temp_sensor_voltages[TOTAL_IC][NUM_TEMPS];
 
 void ltc6811_init(volatile uint8_t* cs_port, uint8_t cs_pin);
 // Half-duplex SPI write, then read
@@ -58,7 +72,38 @@ void _ltc6811_rdcv_reg(uint8_t reg, //Determines which cell voltage register is 
                         uint8_t *data //An array of the unparsed cell codes
                     );
 
-void mux_disable(uint8_t total_ic, uint8_t i2c_adress);
+
+//Start a GPIO and Vref2 Conversion
+// Just write the ADAX command
+void ltc6811_adax(
+        uint8_t MD, //ADC Mode
+        uint8_t CHG //GPIO Channels to be measured)
+    );
+
+/*
+   The function is used to parse the AUX responses from the LTC6811. This function will send the requested
+   read commands parse the data and store the gpio voltages in aux_voltages variable
+   */
+int8_t ltc6811_rdaux(uint8_t reg, //Determines which GPIO voltage register is read back.
+        uint8_t total_ic,//the number of ICs in the system
+        uint16_t aux_voltages[][NUM_AUX_CH]//A two dimensional array of the gpio voltages.
+    );
+
+void _ltc6811_rdaux_reg(uint8_t reg, //Determines which GPIO voltage register is read back
+        uint8_t total_ic, //The number of ICs in the system
+        uint8_t *data //Array of the unparsed auxiliary codes
+        );
+/*
+   Writes the COMM registers of a ltc6811 daisy chain
+   */
+void wrcomm(uint8_t total_ic, //The number of ICs being written to
+            uint8_t comm[][6] //A two dimensional array of the comm data that will be written
+            );
+
+void stcomm(void);
+
+
+void mux_disable(uint8_t total_ic, uint8_t i2c_address);
 
 void mux_set_channel(uint8_t total_ic, uint8_t i2c_address, uint8_t channel);
 
